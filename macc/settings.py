12a9478f5589be828b387a53d05e809b6cea50f8
iamxasan1 <iamxasan2>
2020-11-06 11:21:30 -0800

    socond commit

--- macc/settings.py
+++ macc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ogrsw&y8=fo^uuy#p)e_^2qfum-gwc2x374dd7wr9e_)mc$+ss'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mcdonaldsss.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
